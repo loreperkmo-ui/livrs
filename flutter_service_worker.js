9be54398863ba410c4ce016355e0a7bf5bb9288c
loreperkmo-ui <loreperkmo@gmail.com>
2026-01-14 12:38:44 -0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -57,7 +57,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "34e5ddff9e1995af664a7bf8da1e8f53
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "afccde2f300b61d5bd9b66db71493e16",
 "/": "afccde2f300b61d5bd9b66db71493e16",
-"main.dart.js": "5a293b09f99bfa9a82e7fbb5c8d82aca",
+"main.dart.js": "7c425f589c7d3c71ece16a03daa26fc9",
 "version.json": "9f952d0182aeec3b80097cdf627e6035"};
 // The application shell files that are downloaded before a service worker can
 // start.
